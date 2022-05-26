f4c3b086610769f366594b8f00d28403c1252afe
Eidamiino <adam.pala199@gmail.com>
2022-05-26 08:57:05 +0200

    basic functionality added

--- FileManager3000/Program.cs
+++ FileManager3000/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace FileManager3000
 {
@@ -6,7 +8,30 @@ namespace FileManager3000
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string path = ReadValue($"Enter directory path: ", $"c:\\Windows\\System32");
+			string extensions = ReadValue($"Enter extensions to search for (separated with ;): ", null);
+
+			extensions.Trim(' ');
+
+			var files = Directory.GetFiles(path);
+			var fileInfos = files.Select(x => new FileInfo(x)).ToList();
+			var onlyType = fileInfos.Where(x => x.Extension == extensions).Select(x=>x).ToList();
+
+			onlyType.ForEach(x=>Console.WriteLine($"File: {x.Name}\tSize: {x.Length/1024/1024}MB"));
+
+			var fileCount = onlyType.Count();
+			var sumSize = onlyType.Sum(x => x.Length);
+			var avgSize = onlyType.Average(x => x.Length);
+			Console.WriteLine($"Amount of files: {fileCount}\tSum: {sumSize / 1024 / 1024}MB\tAverage:{avgSize / 1024 / 1024}MB");
+		}
+
+		private static string ReadValue(string label,string defaultValue)
+		{
+			Console.Write($"{label} (default: {defaultValue}): ");
+			string value = Console.ReadLine();
+			if (value=="")
+				return defaultValue;
+			return value;
 		}
 	}
 }
